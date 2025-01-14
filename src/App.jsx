362e7f984be14550304fbe9965deb73cf4ac669e
Haraprasad Sahoo <haraprasadsahoo5805@gmail.com>
2025-01-14 11:58:14 +0530

    2nd commite

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+
 import Random from "./components/Random"
 import Tag from "./components/Tag"
 const App = () => {
   return (
-    <div className='w-full h-screen flex flex-col background absolute overflow-hidden items-center'>
-      <h1 className='bg-white rounded-ld relative w-11/12 text-center mt-[40px]  text-4xl font-bold'>RANDON GIF'S</h1>
+    <div className='w-full h-screen flex flex-col background relative overflow-x-hidden items-center'>
+      <h1 className='bg-white rounded-ld relative w-11/12 text-center mt-[40px]  text-4xl font-bold'>RANDON GIF&apos;S</h1>
       <div className='flex flex-col w-full items-center gap-y-10 mt-[30px]'>
         <Random/>
         <Tag/>
